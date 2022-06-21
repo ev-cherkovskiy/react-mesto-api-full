ce9bbf66f152763273594a4855ac78b83403c308
Evgeny Cherkovskiy <ev.cherkovskiy@yandex.ru>
2022-06-21 16:24:16 +0300

    test

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -137,7 +137,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
+      return res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       });
